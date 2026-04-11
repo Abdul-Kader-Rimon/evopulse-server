a62d9f89455dde7c936a56eda0b8fbc06669a193
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2026-04-11 22:07:43 +0600

    added vercel json

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`Server is running at http://localhost:${PORT}`);
+    // });
   } catch (error) {
     console.error("Failed to start server:", error.message);
   }
